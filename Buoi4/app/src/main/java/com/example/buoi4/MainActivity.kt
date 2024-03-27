6831c7a02e8f60a1fef0475cc9aa0e4398b0c64a
Duckhanhh <88675192+Duckhanhh@users.noreply.github.com>
2024-03-27 23:10:27 +0700

    update

---
+++ Buoi4/app/src/main/java/com/example/buoi4/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.buoi4
+
+import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.buoi4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    val list = mutableListOf<User>(
+        User("NKS", "asd"),
+        User("NOK", "asdasd"),
+        User("NKSdf", "asdpppppp"),
+    )
+    private val userAdapter = UserAdapter(list)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.recyclerView.adapter = userAdapter
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+}
